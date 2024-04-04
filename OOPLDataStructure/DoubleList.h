add027b21c1c6d4146959585fa46b458b659caaa
nazarfrazovir <nazarfrazovir@gmail.com>
2024-04-05 01:13:10 +0300

    Add two methods Push_back Push_Front for double-linked list.

--- OOPLDataStructure/DoubleList.h
+++ OOPLDataStructure/DoubleList.h
@@ -23,5 +23,44 @@ private:
 
     std::shared_ptr<Node2<T>> getNode(size_t index) const;
 
+public:
+    DoubleList() : head(nullptr), tail(nullptr), size(0) {}
+
+    //---Метод вставки---
+    void pushBack(const T& value);
+    void pushFront(const T& value);
+
+
+};
+
+template<typename T>
+void DoubleList<T>::pushBack(const T& value) {
+    std::unique_ptr<Node2<T>> newNode = std::make_unique<Node2<T>>();
+    newNode->data = value;
+    newNode->next = nullptr;
+    newNode->prev = tail;
+    if (tail != nullptr) {
+        tail->next = std::move(newNode);
+        tail = tail->next;
+    } else {
+        head = std::move(newNode);
+        tail = head;
+    }
+    size++;
+}
+
+template<typename T>
+void DoubleList<T>::pushFront(const T& value) {
+    std::unique_ptr<Node2<T>> newNode = std::make_unique<Node2<T>>();
+    newNode->data = value;
+    newNode->next = head;
+    newNode->prev = nullptr;
+    if (head != nullptr) {
+        head->prev = std::move(newNode);
+    } else {
+        tail = std::move(newNode);
+    }
+    head = newNode->prev;
+    size++;
 }
 #endif //OOPLDATASTRUCTURE_DOUBLELIST_H
