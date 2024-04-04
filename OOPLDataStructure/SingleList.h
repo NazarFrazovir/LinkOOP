8f31428f6b53c784848e56afad8116fc0e717f45
nazarfrazovir <nazarfrazovir@gmail.com>
2024-04-05 00:43:20 +0300

    Put all code from SingleList.cpp to SingleList.h

--- OOPLDataStructure/SingleList.h
+++ OOPLDataStructure/SingleList.h
@@ -13,6 +13,8 @@ struct Node{
     T data;
     std::shared_ptr<Node<T>> next;
 
+    Node(const T& value) : data(value), next(nullptr) {}
+
 };
 
 template<typename T>
@@ -39,8 +41,8 @@ public:
     void removeAt(size_t index);
 
     //--Методи для роботи з розміром---
-    bool isEmpty();
-    size_t Size();
+    bool isEmpty() const;
+    size_t Size() const;
 
     //---Метод пошуку---
     bool Find(const T& value)const;
@@ -56,6 +58,122 @@ public:
     }
 
 };
+
+
+template<typename T>
+void SingleList<T>::pushBack(const T& value) {
+    std::unique_ptr<Node<T>> newNode = std::make_unique<Node<T>>(value);
+    if (head == nullptr) {
+        head = std::move(newNode);
+    } else {
+        std::shared_ptr<Node<T>> current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = std::move(newNode);
+    }
+    size++;
+}
+
+template<typename T>
+void SingleList<T>::pushFront(const T& value) {
+    std::unique_ptr<Node<T>> newNode = std::make_unique<Node<T>>(value);
+    newNode->next = std::move(head);
+    head = std::move(newNode);
+    size++;
+}
+
+template<typename T>
+void SingleList<T>::popBack() {
+    if (head == nullptr)
+        throw std::out_of_range("List is empty");
+    if (head->next == nullptr) {
+        head.reset();
+    } else {
+        std::shared_ptr<Node<T>> current = head;
+        while (current->next->next != nullptr) {
+            current = current->next;
+        }
+        current->next.reset();
+    }
+    size--;
+}
+
+template<typename T>
+void SingleList<T>::popFront() {
+    if (head == nullptr)
+        throw std::out_of_range("List is empty");
+    head = std::move(head->next);
+    size--;
+}
+
+template<typename T>
+T& SingleList<T>::operator[](size_t index) const {
+    if (index >= size)
+        throw std::out_of_range("Index out of range");
+    std::shared_ptr<Node<T>> current = head;
+    for (size_t i = 0; i < index; ++i) {
+        current = current->next;
+    }
+    return current->data;
+}
+
+template<typename T>
+void SingleList<T>::insertAt(size_t index, const T& value) {
+    if (index > size)
+        throw std::out_of_range("Index out of range");
+    if (index == 0) {
+        pushFront(value);
+    } else {
+        std::shared_ptr<Node<T>> newNode = std::make_shared<Node<T>>(value);
+        std::shared_ptr<Node<T>> current = head;
+        for (size_t i = 0; i < index - 1; ++i) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+        size++;
+    }
+}
+
+template<typename T>
+void SingleList<T>::removeAt(size_t index) {
+    if (index >= size)
+        throw std::out_of_range("Index out of range");
+    if (index == 0) {
+        popFront();
+    } else {
+        std::shared_ptr<Node<T>> current = head;
+        for (size_t i = 0; i < index - 1; ++i) {
+            current = current->next;
+        }
+        current->next = current->next->next;
+        size--;
+    }
+}
+
+template<typename T>
+size_t SingleList<T>::Size() const {
+    return size;
+}
+
+template<typename T>
+bool SingleList<T>::isEmpty() const {
+    return size == 0;
+}
+
+template<typename T>
+bool SingleList<T>::Find(const T& value) const {
+    std::shared_ptr<Node<T>> current = head;
+    while (current != nullptr) {
+        if (current->data == value) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
 
 
 #endif //OOPLDATASTRUCTURE_SINGLELIST_H
