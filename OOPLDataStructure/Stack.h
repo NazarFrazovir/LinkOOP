c847b0abed2a26c77b2b3065ca5d367a9f6eba2e
nazarfrazovir <nazarfrazovir@gmail.com>
2024-04-12 00:17:04 +0300

    Add all methods for Stack and Stack(array), for Example: push,pop,isEmpty,peek,size.

--- OOPLDataStructure/Stack.h
+++ OOPLDataStructure/Stack.h
@@ -15,5 +15,37 @@ private:
 public:
     Stack() : list() {}
 
+    bool IsEmpty() const{
+        return list.isEmpty();
+    }
+
+    T Peek()const{
+        if (list.isEmpty()){
+            throw std::out_of_range("Stack is empty");
+        }
+        return list[Size() - 1];
+    }
+
+    void Push(const T& value){
+        list.pushBack(value);
+    }
+
+    T Pop(){
+        if (IsEmpty()){
+            throw std::underflow_error("Stack is empty");
+        }
+        T value = list[Size() - 1];
+        list.popBack();
+        return value;
+    }
+
+    size_t Size()const{
+        return list.Size();
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Stack<T>& stack) {
+        os << stack.list;
+        return os;
+    }
 };
 #endif //OOPLDATASTRUCTURE_STACK_H
--- OOPLDataStructure/StackArray.h
+++ OOPLDataStructure/StackArray.h
@@ -10,8 +10,18 @@ template<typename T>
 class StackArray{
 private:
     T* elements;
-    size_t capacity;
-    size_t top;
+    size_t capacity;        // Розмір
+    size_t top;         // Веерхній елемент стека
+
+    void Resize(size_t newCapacity) {
+        T *newItems = new T[newCapacity];
+        for (size_t i = 0; i < top; ++i) {
+            newItems[i] = elements[i];
+        }
+        delete[] elements;
+        elements = newItems;
+        capacity = newCapacity;
+    }       // Зміна розміра масиву
 public:
 
     StackArray(size_t initialCapacity = 10):capacity(initialCapacity),top(0){
@@ -20,6 +30,49 @@ public:
     ~StackArray(){
         delete[] elements;
     }
+
+    bool IsEmpty() const {
+        return top == 0;
+    }
+
+    T Peek() const {
+        if (IsEmpty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+        return elements[top - 1];
+    }
+
+
+    void Push(const T& value) {
+        if (top == capacity) {
+            Resize(capacity * 2); // Подвоюємо розмір масиву, якщо стек повний
+        }
+        elements[top++] = value;
+    }
+
+    T Pop() {
+        if (IsEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        T value = elements[--top];
+        if (top > 0 && top == capacity / 4) {
+            Resize(capacity / 2); // Зменшуємо розмір масиву, якщо стек наполовину порожній
+        }
+        return value;
+    }
+
+    size_t Size() const {
+        return top;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const StackArray<T>& stack) {
+        for (size_t i = 0; i < stack.top; ++i) {
+            os << stack.elements[i] << " ";
+        }
+        return os;
+    }
+
+
 
 };
 #endif //OOPLDATASTRUCTURE_STACKARRAY_H
