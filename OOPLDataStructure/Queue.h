d8a4259c0012852d575bca57f87e0b9f44c3f0e5
nazarfrazovir <nazarfrazovir@gmail.com>
2024-04-11 23:48:55 +0300

    Add all methods for Queue and Queue(array) for Example: Enqueue, Dequeue, Peek, IsEmpty, IsFull.

--- OOPLDataStructure/Queue.h
+++ OOPLDataStructure/Queue.h
@@ -13,6 +13,7 @@ template<typename T>
 class Queue{
 private:
     DoubleList<T> list;
+    size_t capacity;
 public:
     Queue() = default;
     Queue(const Queue& other) = default;
@@ -22,5 +23,39 @@ public:
 
     Queue& operator=(const Queue& other) = default;
     Queue& operator=(Queue&& other) noexcept = default;
+
+    // Через двозвязний список:
+    bool isEmpty()const{
+        return list.Size() == 0;
+    }
+    bool isFull()const{
+        return list.Size() == capacity;
+    }
+    void enqueue(const T& value){
+        if (isFull()){
+            throw std::overflow_error("Queue is full");
+        }
+        list.pushBack(value);
+    }
+    T dequeue(){
+        if (isEmpty()){
+            throw std::underflow_error("Queue is empty |'_'|");
+        }
+        T frontValue = list[0];
+        list.popFront();
+        return frontValue;
+    }
+    T Peek()const{
+        if(isEmpty()){
+            throw std::out_of_range("Queue is empty |'_'|");
+        }
+        return list[0];
+    }
+
+
+    friend std::ostream& operator<<(std::ostream& os, const Queue<T>& queue) {
+        os << queue.list;
+        return os;
+    }
 };
 #endif //OOPLDATASTRUCTURE_QUEUE_H
--- OOPLDataStructure/QueueArray.h
+++ OOPLDataStructure/QueueArray.h
@@ -6,7 +6,6 @@
 #define OOPLDATASTRUCTURE_QUEUEARRAY_H
 
 #include <iostream>
-const int SIZE = 5;
 
 template<typename T>
 class QueueArray{
@@ -19,6 +18,57 @@ public:
         front = -1;
         rear = -1;
     }
-    
+
+    bool isEmpty() const {
+        return front == -1 && rear == -1;
+    }
+    bool isFull()const{
+        return rear == SIZE - 1;
+    }
+    void enqueue(T value){
+        if (isFull()){
+            throw std::overflow_error("Queue is full");
+        }
+        if (isEmpty()){
+            front = rear = 0;
+        }else{
+            rear++;
+        }
+        element[rear] = value;
+//        std::cout<<"Enqueued: "<<value<<std::endl;
+    }
+    T dequeue(){
+        if (isEmpty()){
+            throw std::underflow_error("Queue is empty");
+        }
+        T removedElem = element[front];
+        if (front == rear){
+            front = rear = -1;
+        }else{
+            front++;
+        }
+//        std::cout << "Dequeued: " << removedElem << std::endl;
+        return removedElem;
+    }
+    T peek(){
+        if (isEmpty()){
+            throw std::out_of_range("Queue is empty");
+        }
+        return element[front];
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const QueueArray<T>& queue) {
+        if (queue.isEmpty()) {
+            os << "Queue is empty";
+        } else {
+            int i = queue.front;
+            while (i != queue.rear) {
+                os << queue.element[i] << " ";
+                i = (i + 1) % SIZE;
+            }
+            os << queue.element[i];
+        }
+        return os;
+    }
 };
 #endif //OOPLDATASTRUCTURE_QUEUEARRAY_H
